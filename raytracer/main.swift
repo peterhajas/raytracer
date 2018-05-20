fe5146ccc1fd585e4b9d320b19316bb3acfe18ca
Peter Hajas <peterhajas@gmail.com>
2018-05-19 21:54:25 -0700

    Add another sphere

--- raytracer/main.swift
+++ raytracer/main.swift
@@ -9,14 +9,14 @@
 import Foundation
 
 let sampleSphere = Sphere(center: Vector3D(a: 100, b: -100, c: 1000), radius: 200)
+let sampleSphere2 = Sphere(center: Vector3D(a: -100, b: 100, c: 1000), radius: 150)
 let camera = Vector3D(a: 0, b: 0, c: -1000)
-let ground = Plane3D(center: Vector3D(-10), normal: Vector3D(a: 0, b: 1, c: 0))
 let light = Vector3D(a: -5000, b: 5000, c: -5000)
 
 // Put the image plane a bit in front of the camera
 let imagePlane = ImagePlane3D(center: Vector3D(a: 0, b: 0, c: 0), width: 500, height: 500)
 
-let world = World(camera: camera, imagePlane: imagePlane, objects: [sampleSphere, ground], lights: [light])
+let world = World(camera: camera, imagePlane: imagePlane, objects: [sampleSphere, sampleSphere2], lights: [light])
 
 let image = imageFromWorld(world, size: CGSize(width: 1000, height: 1000))
 print(image)
